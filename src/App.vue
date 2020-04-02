bce3b9cbd4dec60933e489f3eb015b29f707aa7b
dai <785228252@qq.com>
2020-04-02 21:12:47 +0800

    修改测试

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
+  <div>
+    <img src="./assets/logo.png">
+    <router-view/>
+    <!--<div id="app-1">
+          {{ message }}
+    </div>
+    &lt;!&ndash; v-for &ndash;&gt;
+    <div id="app-2">
+          <ul>
+            <li v-for = "input in inputs">
+              {{input}}
+            </li>
+          </ul>
+    </div>
+    <div id="hw" >
+        12323323
+    </div>-->
+    <div class="hw1" >
+      12323323
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message : 'Hello word!',
+      inputs:['text','select','radio']
+    }
+  }
+}
+</script>
+
+<style>
+.app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.hw1{
+  height: 100px;
+  width: 100px;
+  background-color: red;
+}
+</style>
---
+++ src/BackDataSet.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="back_data_set">
+      <h1>
+        资料设置
+      </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BackDataSet"
+    }
+</script>
+
+<style scoped>
+  .back_data_set {
+    /*position: absolute;*/
+    width: 100%;
+    height: 100%;
+    background-color: #42b983;
+    text-align: center;
+  }
+
+</style>
